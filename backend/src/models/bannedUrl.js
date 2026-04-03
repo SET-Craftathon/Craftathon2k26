c07f73d767e60df1d51c8679c78d37d3018b81a1
varun-ai69 <kushwahavarun86@gmail.com>
2026-04-04 03:51:20 +0530

    bannedUrl Model

---
+++ backend/src/models/bannedUrl.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const bannedUrlSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    domain: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    reportCount: {
+      type: Number,
+      default: 1,
+    },
+
+  
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+bannedUrlSchema.index({ domain: 1 });
+bannedUrlSchema.index({ url: 1 });
+
+const BannedUrl = mongoose.model("BannedUrl", bannedUrlSchema);
+
+module.exports = BannedUrl;
